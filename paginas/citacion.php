f48f51ac054bfddcf17246229caf7b871aab6038
TakyAndres <82551421+TakyAndres@users.noreply.github.com>
2021-04-27 09:58:30 -0500

    xd
    xd

--- paginas/citacion.php
+++ paginas/citacion.php
@@ -5,7 +5,14 @@ require_once ('C:\xampp\htdocs\5L\PROYECTO\f2\paginas\menu.php');
 <!DOCTYPE html>
 <html lang="es">
 
-
+<div class="h2"> Esta página web encontraras información sobre el proceso de vacunación de la EPS SANITAS.<br> Aquí podrás consultar información a cerca de los afiliados, agendamiento de citas, 
+      fecha y lugar de vacunación contra el COVID 19.<br> Las vacunas están próximas a llegar al 
+      país y daremos inicio al proceso de vacunación. <br>Es muy importante mantener tus datos al día, 
+      esto nos permitirá estar en contacto contigo <br> para informarte cuando sea el momento de tu 
+      vacuna y resolver todas las dudas que puedas tener al respecto.
+      <p></p>
+      <img src="/5L/Proyecto/f2/anexos/img/VACUNA.gif" id="doctor" >
+      
 <p> &nbsp;</p>
 </html>
       
